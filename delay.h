d04190b5782d276f6617bea223cf1bbe0b037be0
zhangyuhu <zhangyuhude@163.com>
2017-03-12 01:18:32 +0800

    Deleted some file

--- delay.h
+++
@@ -1,6 +0,0 @@
-#ifndef __delay_H__
-#define __delay_H__
-
-void delayms(int i);
-void delays(int i);
-#endif
--- is_compatible.h
+++
@@ -1,8 +0,0 @@
-#ifndef __IS_COMPATIBLE_H__
-#define __IS_COMPATIBLE_H__
-
-#define bool char
-#define true 1
-#define false 0
-
-#endif
