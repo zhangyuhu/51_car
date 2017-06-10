62ef6eb4f771bfbea11f114d6d4845618024cb77
unknown <>
2017-06-10 23:51:01 +0800

    Adjust format.

--- framework/delay/delay.c
+++ framework/delay/delay.c
@@ -6,22 +6,22 @@
 void delayms(int i)  
 {
     unsigned char a,b,c;
-	  while(i--)
-		{
+    while(i--)
+    {
     for(c=1;c>0;c--)
         for(b=142;b>0;b--)
             for(a=2;a>0;a--);
-		}
+    }
 }
 
 void delays(int i)    
 {
     unsigned char a,b,c;
-	while(i--)
-	{
+    while(i--)
+    {
     for(c=167;c>0;c--)
         for(b=171;b>0;b--)
             for(a=16;a>0;a--);
-    _nop_();  //if Keil,require use intrins.h
-	}
+              _nop_();  //if Keil,require use intrins.h
+    }
 }
